eea1ecb4ac44ea42f5a7c3467e98170e8c55ad55
gordo <gordonbchen@gmail.com>
2026-09-20 19:45:09 -0400

    lox -> lex

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,10 @@
 let run src =
     let chars = List.init (String.length src) (String.get src) in
     let line = ref 1 in
-    let (tokens, errors) = Olox.Lox.scan chars [] [] line in
+    let (tokens, errors) = Olox.Lex.scan chars [] [] line in
     let errored = ref (not (List.is_empty errors)) in
     List.iter (fun msg -> print_string msg) errors;
-    List.iter (fun tok -> Printf.printf "%s, " @@ Olox.Lox.show_token tok) tokens;
+    List.iter (fun tok -> Printf.printf "%s, " @@ Olox.Lex.show_token tok) tokens;
     print_char '\n';
 
     (match Olox.Parse.parse tokens with
--- lib/parse.ml
+++ lib/parse.ml
@@ -1,16 +1,16 @@
 type expr =
-  | Literal of Lox.token  (* TODO: should this be a new literal type? *)
-  | Unary of {op: Lox.token; right: expr}
-  | Binary of {left: expr; op: Lox.token; right: expr}
-  | Primary of Lox.token
+  | Literal of Lex.token  (* TODO: should this be a new literal type? *)
+  | Unary of {op: Lex.token; right: expr}
+  | Binary of {left: expr; op: Lex.token; right: expr}
+  | Primary of Lex.token
   | Grouping of expr
 
 let rec expr_to_str expr = match expr with
-  | Literal l -> Lox.show_token l
-  | Unary {op; right} -> Lox.show_token op ^ " " ^ expr_to_str right
-  | Binary {left; op; right} -> expr_to_str left ^ " " ^ Lox.show_token op
+  | Literal l -> Lex.show_token l
+  | Unary {op; right} -> Lex.show_token op ^ " " ^ expr_to_str right
+  | Binary {left; op; right} -> expr_to_str left ^ " " ^ Lex.show_token op
                                                  ^ " " ^ expr_to_str right
-  | Primary p -> Lox.show_token p
+  | Primary p -> Lex.show_token p
   | Grouping expr -> "(" ^ expr_to_str expr ^ ")"
 
 
@@ -47,28 +47,28 @@ let parse_binary parse_func ops tokens =
 let rec parse tokens = parse_equality tokens
 
 and parse_equality tokens = parse_binary parse_comparison
-  [Lox.TBangEqual; Lox.TEqualEqual] tokens 
+  [Lex.TBangEqual; Lex.TEqualEqual] tokens 
 
 and parse_comparison tokens = parse_binary parse_term
-  [Lox.TLess; Lox.TLessEqual; Lox.TGreaterEqual; Lox.TGreater] tokens
+  [Lex.TLess; Lex.TLessEqual; Lex.TGreaterEqual; Lex.TGreater] tokens
 
-and parse_term tokens = parse_binary parse_factor [Lox.TMinus; Lox.TPlus] tokens
+and parse_term tokens = parse_binary parse_factor [Lex.TMinus; Lex.TPlus] tokens
 
-and parse_factor tokens = parse_binary parse_unary [Lox.TSlash; Lox.TStar] tokens
+and parse_factor tokens = parse_binary parse_unary [Lex.TSlash; Lex.TStar] tokens
 
 and parse_unary tokens = match tokens with
-  | (Lox.TBang | Lox.TMinus) as op :: toks ->
+  | (Lex.TBang | Lex.TMinus) as op :: toks ->
     let* (right, rem) = parse_unary toks in (Ok (Unary {op; right}), rem)
   | _ -> parse_primary tokens
 
 and parse_primary tokens = match tokens with
-  | (Lox.TNum _ | Lox.TString _ | Lox.TTrue | Lox.TFalse | Lox.TNil) as t :: rem ->
+  | (Lex.TNum _ | Lex.TString _ | Lex.TTrue | Lex.TFalse | Lex.TNil) as t :: rem ->
     (Ok (Primary t), rem)
-  | Lox.TLeftParen :: toks ->
+  | Lex.TLeftParen :: toks ->
     let* (expr, toks1) = parse toks in (
       match toks1 with
-        | Lox.TRightParen :: rem -> (Ok (Grouping expr), rem)
+        | Lex.TRightParen :: rem -> (Ok (Grouping expr), rem)
         | _ -> (Error "Expected ')' after expression.", toks1)
     )
-  | t :: rem -> (Error ("Expected to read a primary but found a " ^ Lox.show_token t), tokens)
+  | t :: rem -> (Error ("Expected to read a primary but found a " ^ Lex.show_token t), tokens)
   | [] -> (Error "Expected to read a primary", [])
